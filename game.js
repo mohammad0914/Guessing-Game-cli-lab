9c9eefacc8e45a74882ed3cf18562371b0913815
Jimmy Byess <jimmy@skelios.com>
2020-11-05 15:14:36 -0500

    add game comments

--- game.js
+++ game.js
@@ -1,20 +1,51 @@
-
 const rls = require('readline-sync')
 
+/**
+ * Starts the game by prompting the user if they want to play
+ * Calls either gameLoop() or quitGame()
+ * 
+ * @returns {undefined}
+ */
 const startGame = () => {
 
 }
 
+/**
+ * Logs "Goodbye!"
+ * Calls process.exit() to quit the game
+ * 
+ * @returns {undefined}
+ */
 const quitGame = () => {
 
 }
 
+/**
+ * Controls the flow of the game.
+ * Should prompt the user to play again once all
+ * guesses have been made or correct answer guessed
+ * 
+ * @returns {undefined}
+ */
 const gameLoop = () => {
 
 }
 
+
+/***
+ * Generates a random number 
+ *
+ * @returns {number} - a number between 1 and 1000
+ */
+const generateRandomNumber = () => {
+
+}
+
+startGame()
+
 module.exports = {
   startGame,
   quitGame,
-  gameLoop
+  gameLoop,
+  generateRandomNumber
 }
